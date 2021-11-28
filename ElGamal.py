197c4035a8a8ea99a1ce6f4e6d6cec524e70557b
lanAnhTran <lananh30thang07@gmail.com>
2021-11-28 07:30:36 +0700

    add prime

---
+++ ElGamal.py
@@ -0,0 +1,242 @@
+import random
+import sys
+
+class PrivateKey(object):
+	def __init__(self, p=None, g=None, x=None, iNumBits=0):
+		self.p = p
+		self.g = g
+		self.x = x
+		self.iNumBits = iNumBits
+
+class PublicKey(object):
+	def __init__(self, p=None, g=None, h=None, iNumBits=0):
+		self.p = p
+		self.g = g
+		self.h = h
+		self.iNumBits = iNumBits
+
+def gcd( a, b ):
+		while b != 0:
+			c = a % b
+			a = b
+			b = c
+		return a
+
+# trả về base^exp mod modulus
+def modexp( base, exp, modulus ):
+		return pow(base, exp, modulus)
+
+def SS( num, iConfidence ):
+		for i in range(iConfidence):
+				a = random.randint( 1, num-1 )
+				if gcd( a, num ) > 1:
+						return False
+				if not jacobi( a, num ) % num == modexp ( a, (num-1)//2, num ):
+						return False
+		return True
+
+# tính jacobi của a, n
+def jacobi( a, n ):
+		if a == 0:
+				if n == 1:
+						return 1
+				else:
+						return 0
+		elif a == -1:
+				if n % 2 == 0:
+						return 1
+				else:
+						return -1
+		elif a == 1:
+				return 1
+		elif a == 2:
+				if n % 8 == 1 or n % 8 == 7:
+						return 1
+				elif n % 8 == 3 or n % 8 == 5:
+						return -1
+		elif a >= n:
+				return jacobi( a%n, n)
+		elif a%2 == 0:
+				return jacobi(2, n)*jacobi(a//2, n)
+		else:
+				if a % 4 == 3 and n%4 == 3:
+						return -1 * jacobi( n, a)
+				else:
+						return jacobi(n, a )
+
+# tìm một số g là thành phần nguyên thủy của số nguyên tố p
+# thuật toán được sử dụng trong hàm: http://modular.math.washington.edu/edu/2007/spring/ent/ent-html/node31.html
+def find_primitive_root( p ):
+		if p == 2:
+				return 1
+		# p-1 = 2 * (p-1)/2
+		p1 = 2
+		p2 = (p-1) // p1
+
+		# thử g ngẫu nhiên trong khoảng (2, p-1)
+		# cho đến khi tìm được 1 số g là thành phần nguyên thủy của p
+		while( 1 ):
+				g = random.randint( 2, p-1 )
+				# g is a primitive root if for all prime factors of p-1, p[i]
+				# g^((p-1)/p[i]) (mod p) is not congruent to 1
+				if not (modexp( g, (p-1)//p1, p ) == 1):
+						if not modexp( g, (p-1)//p2, p ) == 1:
+								return g
+
+# tìm một số nguyên tố với số bit = iNumBits
+def find_prime(iNumBits, iConfidence):
+		# thử ngẫu nhiên cho đến khi tìm được một số
+		while(1):
+				p = random.randint( 2**(iNumBits-2), 2**(iNumBits-1) )
+				# p phải là số lẻ
+				while( p % 2 == 0 ):
+						p = random.randint(2**(iNumBits-2),2**(iNumBits-1))
+
+				# keep doing this if the solovay-strassen test fails
+				while( not SS(p, iConfidence) ):
+						p = random.randint( 2**(iNumBits-2), 2**(iNumBits-1) )
+						while( p % 2 == 0 ):
+								p = random.randint(2**(iNumBits-2), 2**(iNumBits-1))
+
+				# if p is prime compute p = 2*p + 1
+				# if p is prime, we have succeeded; else, start over
+				p = p * 2 + 1
+				if SS(p, iConfidence):
+						return p
+
+# mã hóa message bytes --> integers
+def encode(sPlaintext, iNumBits):
+		byte_array = bytearray(sPlaintext, 'utf-16')
+		z = []
+		k = iNumBits//8
+		j = -1 * k
+		num = 0
+
+		for i in range( len(byte_array) ):
+				if i % k == 0:
+						j += k
+						num = 0
+						z.append(0)
+				z[j//k] += byte_array[i]*(2**(8*(i%k)))
+
+		# ví dụ
+		# 		n = 24, k = n / 8 = 3
+		# 		z[0] = (tổng từ i = 0 đến i = k)m[i]*(2^(8*i))
+		# 		m[i] byte thứ i của tin nhắn is
+
+		return z
+
+# giải mã integers -->  message bytes
+def decode(aiPlaintext, iNumBits):
+		bytes_array = []
+
+		k = iNumBits//8
+
+		for num in aiPlaintext:
+				for i in range(k):
+						temp = num
+						for j in range(i+1, k):
+								temp = temp % (2**(8*j))
+						letter = temp // (2**(8*i))
+						bytes_array.append(letter)
+						num = num - (letter*(2**(8*i)))
+
+		# example
+		# if "You" were encoded.
+		# Letter        #ASCII
+		# Y              89
+		# o              111
+		# u              117
+		# if the encoded integer is 7696217 and k = 3
+		# m[0] = 7696217 % 256 % 65536 / (2^(8*0)) = 89 = 'Y'
+		# 7696217 - (89 * (2^(8*0))) = 7696128
+		# m[1] = 7696128 % 65536 / (2^(8*1)) = 111 = 'o'
+		# 7696128 - (111 * (2^(8*1))) = 7667712
+		# m[2] = 7667712 / (2^(8*2)) = 117 = 'u'
+
+		decodedText = bytearray(b for b in bytes_array).decode('utf-16')
+		return decodedText
+
+# tạo khóa công khai K1 (p, g, h) và khóa bí mật K2 (p, g, x)
+def generate_keys(iNumBits=256, iConfidence=32):
+		# p: số nguyên tố
+		# g: thành phần nguyên tố của p
+		# x: số ngẫu nhiên trên đoạn (0, p-1)
+		# h = g ^ x mod p
+		p = find_prime(iNumBits, iConfidence)
+		g = find_primitive_root(p)
+		g = modexp( g, 2, p )
+		x = random.randint( 1, (p - 1) // 2 )
+		h = modexp( g, x, p )
+
+		publicKey = PublicKey(p, g, h, iNumBits)
+		privateKey = PrivateKey(p, g, x, iNumBits)
+
+		return {'privateKey': privateKey, 'publicKey': publicKey}
+
+# def is_iNumBits_primeNumber(p, iNumBits):
+# 	return true if p is a prime number and number of bits of p == iNumBits
+
+# def is_primitive_root(g, p):
+# 	return true if g is primitive root of p
+
+
+
+# mã hóa bản tin sPlaintext với khóa key
+def encrypt(key, sPlaintext):
+		z = encode(sPlaintext, key.iNumBits)
+
+		# cipher_pairs sẽ gồm các cặp (c, d) ứng với mỗi số nguyên trong mảng z
+		cipher_pairs = []
+		
+		for i in z:
+				# y: số ngẫu nhiên trên đoạn (0, p-1)
+				y = random.randint( 0, key.p )
+				# c = g^y mod p
+				c = modexp( key.g, y, key.p )
+				# d = ih^y mod p
+				d = (i*modexp( key.h, y, key.p)) % key.p
+				# thêm cặp (c, d) vào list cipher_pairs
+				cipher_pairs.append( [c, d] )
+
+		encryptedStr = ""
+		for pair in cipher_pairs:
+				encryptedStr += str(pair[0]) + ' ' + str(pair[1]) + ' '
+	
+		return encryptedStr
+
+
+# giải mã với cặp mã hóa cipher với khóa bí mật key
+def decrypt(key, cipher):
+		plaintext = []
+
+		cipherArray = cipher.split()
+		if (not len(cipherArray) % 2 == 0):
+				return "Malformed Cipher Text"
+		for i in range(0, len(cipherArray), 2):
+				c = int(cipherArray[i])
+				d = int(cipherArray[i+1])
+
+				# s = c^x mod p
+				s = modexp( c, key.x, key.p )
+				# plaintext integer = ds^-1 mod p
+				plain = (d*modexp( s, key.p-2, key.p)) % key.p
+				
+				plaintext.append( plain )
+
+		decryptedText = decode(plaintext, key.iNumBits)
+
+		# loại bỏ bytes null
+		decryptedText = "".join([ch for ch in decryptedText if ch != '\x00'])
+
+		return decryptedText
+
+# hàm kiểm tra
+def test(message):
+		assert (sys.version_info >= (3,4))
+		keys = generate_keys()
+		priv = keys['privateKey']
+		pub = keys['publicKey']
+		cipher = encrypt(pub, message)
+		plain = decrypt(priv, cipher)
+		return message == plain
---
+++ main.py
@@ -0,0 +1,192 @@
+import random
+import ElGamal
+from tkinter import *
+from tkinter import ttk, messagebox
+from PIL import ImageTk, Image
+
+# setup interface
+root = Tk()
+root.title('ElGamal Cryptosystem')
+root.geometry('820x700')
+root.resizable(False, False)
+
+backgound_image = ImageTk.PhotoImage(Image.open('bg.png'))
+backgound_label = Label(image=backgound_image, height=700, width=820)
+backgound_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+credits_label = Label(text='ElGamal Cryptosystem by Group 5')
+credits_label.place(relx=1.0, rely=1.0, anchor='se')
+
+#title UI
+title = Label(root, text='Chữ ký ElGamal', font=('TkDefaultFont', 20))
+title.place(x=300, y=10)
+
+# keys UI
+key_label = Label(root, text='Khóa')
+key_label.place(x=40, y=60)
+
+# số bit của p
+bitNum_label = Label(root, text='Số bit của p')
+bitNum_label.place(x=190, y=64)
+
+# các lựa chọn cho số bit của p
+option= [100, 500, 1000]
+bitNum_value= IntVar()
+bitNum_value.set(option[0])
+bitNum_option = OptionMenu(root, bitNum_value, *option)
+bitNum_option.place(x=270, y=60)
+
+# anpha
+anpha_label = Label(root, text='anpha = ')
+anpha_label.place(x=110, y=108)
+
+anpha_text = Text(root, width=65, height=2)
+anpha_text.place(x=170, y=100)
+
+# p
+p_label = Label(root, text='p = ')
+p_label.place(x=135, y=185)
+
+p_text = Text(root, width=65, height=4)
+p_text.place(x=170, y=160)
+
+# a
+a_label = Label(root, text='a = ')
+a_label.place(x=135, y=265)
+
+a_text = Text(root, width=65, height=3)
+a_text.place(x=170, y=250)
+
+# tạo khóa
+def keys_generate():
+    # xóa khóa cũ
+    result_text.delete('1.0', END)
+    cipher_message_text.delete('1.0', END)
+    decrypt_message_text.delete('1.0', END)
+
+    # tạo khóa mới
+    p = ElGamal.find_prime(bitNum_value.get(), 32)
+    p_text.delete('1.0', END)
+    p_text.insert(END, p)
+    g = ElGamal.find_primitive_root(p)
+    anpha_text.delete('1.0', END)
+    anpha_text.insert(END, g)
+    x = random.randint( 1, (p - 1) // 2 )
+    a_text.delete('1.0', END)
+    a_text.insert(END, x)
+
+keys_generate_button = Button(text='Tạo khóa', bg='white', fg='black', command=keys_generate)
+keys_generate_button.place(x=500, y=64)
+
+# kiểm tra khóa đã thỏa mãn điều kiện hay chưa
+def keys_check():
+    messagebox.showerror("Error", "Check keys is not active now, please try again later")
+
+keys_check_button = Button(text='Kiểm tra khóa', bg='white', fg='black', command=keys_check)
+keys_check_button.place(x=600, y=64)
+
+# separator
+separator = ttk.Separator(root, orient='horizontal')
+separator.place(x=70, y=320, relwidth=0.8)
+
+# bản tin gốc
+original_message_label = Label(root, text='Tin nhắn gốc')
+original_message_label.place(x=40, y=365)
+
+original_message_text = Text(root, width=65, height=5)
+original_message_text.place(x=170, y=340)
+
+# mã hóa bản tin gốc
+def encrypt():
+    
+    # xóa văn bản mã hóa cũ
+    result_text.delete('1.0', END)
+    cipher_message_text.delete('1.0', END)
+    decrypt_message_text.delete('1.0', END)
+    
+    # lấy khóa
+    iNumBits = bitNum_value.get()
+    p = int(p_text.get('1.0', "end-1c"))
+    g = int(anpha_text.get('1.0', "end-1c"))
+    x = int(a_text.get('1.0', "end-1c"))
+    h = ElGamal.modexp( g, x, p )
+    publicKey = ElGamal.PublicKey(p, g, h, iNumBits)
+    
+    # lấy bản tin gốc
+    message = original_message_text.get('1.0', "end-1c")
+    
+    # mã hóa và hiển thị bản tin mã hóa
+    cipher_message = ElGamal.encrypt(publicKey, message)
+    cipher_message_text.insert(END, cipher_message)
+
+encrypt_button = Button(text='Mã hóa', bg='white', fg='black', command=encrypt)
+encrypt_button.place(x=730, y=365)
+
+# bản tin mã hóa
+cipher_message_label = Label(root, text='Bản mã hóa')
+cipher_message_label.place(x=40, y=475)
+
+cipher_message_text = Text(root, width=65, height=5)
+cipher_message_text.place(x=170, y=450)
+
+# giải mã
+def decrypt():
+
+    # xóa bản tin giải mã
+    result_text.delete('1.0', END)
+    decrypt_message_text.delete('1.0', END)
+
+    # lấy khóa
+    iNumBits = bitNum_value.get()
+    p = int(p_text.get('1.0', "end-1c"))
+    g = int(anpha_text.get('1.0', "end-1c"))
+    x = int(a_text.get('1.0', "end-1c"))
+    privateKey = ElGamal.PrivateKey(p, g, x, iNumBits)
+    
+    # lấy bản tin mã hóa
+    cipher_message = cipher_message_text.get('1.0', "end-1c")
+    
+    # giải mã và hiển thị bản tin sau khi giải mã 
+    decrypt_message = ElGamal.decrypt(privateKey, cipher_message)
+    decrypt_message_text.insert(END, decrypt_message)
+
+decrypt_button = Button(text='Giải mã', bg='white', fg='black', command=decrypt)
+decrypt_button.place(x=730, y=475)
+
+# bản tin giải mã
+decrypt_message_label = Label(root, text='Bản giải mã')
+decrypt_message_label.place(x=40, y=590)
+
+decrypt_message_text = Text(root, width=65, height=5)
+decrypt_message_text.place(x=170, y=560)
+
+# kiểm tra bản tin giải mã có giống với bản tin gốc hay không
+def test():
+
+    # xóa kết quả so sánh cũ
+    result_text.delete('1.0', END)
+    
+    # lấy bản tin gốc
+    original_message = original_message_text.get('1.0', "end-1c")
+    
+    # lấy bản tin giải mã
+    decrypt_message = decrypt_message_text.get('1.0', "end-1c")
+    
+    # so sánh và hiển thị kết quả
+    if original_message == decrypt_message:
+        result_text.insert(END, 'đúng' )
+    else:
+        result_text.insert(END, 'sai')
+
+test_button = Button(text='Kiểm tra', bg='white', fg='black', command=test)
+test_button.place(x=730, y=590)
+
+# kết quả kiểm tra
+result_label = Label(root, text='Kết quả')
+result_label.place(x=370, y=660)
+
+result_text = Text(root, width=10, height=1)
+result_text.place(x=430, y=660)
+
+
+root.mainloop()
