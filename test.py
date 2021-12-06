e846a8f5fe70af9dbdb3d9855aae52773ac1e643
Trong Nguyen <trong.nguyen@seltec.io>
2021-12-06 16:17:18 +0700

    Minor fixes test with updated functions

--- test.py
+++ test.py
@@ -10,45 +10,131 @@ import ElGamal
 
 class TestElGamalMethods(unittest.TestCase):
 
-    def test_generate_prime_length_100(self):
+    def test_generate_prime_length_100_1(self):
         prime = gp.generatePrime(100, 23)
         self.assertEqual(len(bin(prime)) - 2, 100)
         self.assertEqual(number.isPrime(prime), True)
 
-    def test_generate_prime_length_500(self):
+    def test_generate_prime_length_100_2(self):
+        prime = gp.generatePrime(100, 23)
+        self.assertEqual(len(bin(prime)) - 2, 100)
+        self.assertEqual(number.isPrime(prime), True)
+
+    def test_generate_prime_length_100_3(self):
+        prime = gp.generatePrime(100, 23)
+        self.assertEqual(len(bin(prime)) - 2, 100)
+        self.assertEqual(number.isPrime(prime), True)
+
+    def test_generate_prime_length_500_1(self):
         prime = gp.generatePrime(500, 23)
         self.assertEqual(len(bin(prime)) - 2, 500)
         self.assertEqual(number.isPrime(prime), True)
 
-    def test_generate_prime_length_1000(self):
+    def test_generate_prime_length_500_2(self):
+        prime = gp.generatePrime(500, 23)
+        self.assertEqual(len(bin(prime)) - 2, 500)
+        self.assertEqual(number.isPrime(prime), True)
+
+    def test_generate_prime_length_500_3(self):
+        prime = gp.generatePrime(500, 23)
+        self.assertEqual(len(bin(prime)) - 2, 500)
+        self.assertEqual(number.isPrime(prime), True)
+
+    def test_generate_prime_length_1000_1(self):
         prime = gp.generatePrime(1000, 23)
         self.assertEqual(len(bin(prime)) - 2, 1000)
         self.assertEqual(number.isPrime(prime), True)
 
-    def test_sign_message(self):
+    def test_generate_prime_length_1000_2(self):
+        prime = gp.generatePrime(1000, 23)
+        self.assertEqual(len(bin(prime)) - 2, 1000)
+        self.assertEqual(number.isPrime(prime), True)
+
+    def test_generate_prime_length_1000_3(self):
+        prime = gp.generatePrime(1000, 23)
+        self.assertEqual(len(bin(prime)) - 2, 1000)
+        self.assertEqual(number.isPrime(prime), True)
+
+    def test_sign_message_1(self):
         message = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10000))
         p = gp.generatePrime(100, ElGamal.ATTEMPTS)
         alpha = ElGamal.find_primitive_root(p)
         privateKey = ElGamal.PrivateKey(random.randint(2, (p - 1)))
-        publicKey = ElGamal.PublicKey(p, alpha, privateKey)
+        beta = ElGamal.modexp(alpha, privateKey.getvalue(), p)
+        publicKey = ElGamal.PublicKey(p, alpha, beta)
 
         sig_num = ElGamal.SigNum \
                 .of_key(message, privateKey, publicKey, ElGamal.ALPHABET)
 
-        self.assertTrue(sig_num.verify(message, privateKey, publicKey, ElGamal.ALPHABET))
+        self.assertTrue(sig_num.verify(message, publicKey, ElGamal.ALPHABET))
+
+    def test_sign_message_2(self):
+        message = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10000))
+        p = gp.generatePrime(100, ElGamal.ATTEMPTS)
+        alpha = ElGamal.find_primitive_root(p)
+        privateKey = ElGamal.PrivateKey(random.randint(2, (p - 1)))
+        beta = ElGamal.modexp(alpha, privateKey.getvalue(), p)
+        publicKey = ElGamal.PublicKey(p, alpha, beta)
+
+        sig_num = ElGamal.SigNum \
+                .of_key(message, privateKey, publicKey, ElGamal.ALPHABET)
+
+        self.assertTrue(sig_num.verify(message, publicKey, ElGamal.ALPHABET))
+
+    def test_sign_message_3(self):
+        message = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10000))
+        p = gp.generatePrime(100, ElGamal.ATTEMPTS)
+        alpha = ElGamal.find_primitive_root(p)
+        privateKey = ElGamal.PrivateKey(random.randint(2, (p - 1)))
+        beta = ElGamal.modexp(alpha, privateKey.getvalue(), p)
+        publicKey = ElGamal.PublicKey(p, alpha, beta)
+
+        sig_num = ElGamal.SigNum \
+                .of_key(message, privateKey, publicKey, ElGamal.ALPHABET)
+
+        self.assertTrue(sig_num.verify(message, publicKey, ElGamal.ALPHABET))
 
 
-    def test_sign_message_1000(self):
+    def test_sign_message_1000_1(self):
         message = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10000))
         p = gp.generatePrime(1000, ElGamal.ATTEMPTS)
         alpha = ElGamal.find_primitive_root(p)
         privateKey = ElGamal.PrivateKey(random.randint(2, (p - 1)))
-        publicKey = ElGamal.PublicKey(p, alpha, privateKey)
+        beta = ElGamal.modexp(alpha, privateKey.getvalue(), p)
+        publicKey = ElGamal.PublicKey(p, alpha, beta)
 
         sig_num = ElGamal.SigNum \
                 .of_key(message, privateKey, publicKey, ElGamal.ALPHABET)
 
-        self.assertTrue(sig_num.verify(message, privateKey, publicKey, ElGamal.ALPHABET))
+        self.assertTrue(sig_num.verify(message, publicKey, ElGamal.ALPHABET))
+
+
+    def test_sign_message_1000_2(self):
+        message = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10000))
+        p = gp.generatePrime(1000, ElGamal.ATTEMPTS)
+        alpha = ElGamal.find_primitive_root(p)
+        privateKey = ElGamal.PrivateKey(random.randint(2, (p - 1)))
+        beta = ElGamal.modexp(alpha, privateKey.getvalue(), p)
+        publicKey = ElGamal.PublicKey(p, alpha, beta)
+
+        sig_num = ElGamal.SigNum \
+                .of_key(message, privateKey, publicKey, ElGamal.ALPHABET)
+
+        self.assertTrue(sig_num.verify(message, publicKey, ElGamal.ALPHABET))
+
+
+    def test_sign_message_1000_3(self):
+        message = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10000))
+        p = gp.generatePrime(1000, ElGamal.ATTEMPTS)
+        alpha = ElGamal.find_primitive_root(p)
+        privateKey = ElGamal.PrivateKey(random.randint(2, (p - 1)))
+        beta = ElGamal.modexp(alpha, privateKey.getvalue(), p)
+        publicKey = ElGamal.PublicKey(p, alpha, beta)
+
+        sig_num = ElGamal.SigNum \
+                .of_key(message, privateKey, publicKey, ElGamal.ALPHABET)
+
+        self.assertTrue(sig_num.verify(message, publicKey, ElGamal.ALPHABET))
 
 
 if __name__ == '__main__':
